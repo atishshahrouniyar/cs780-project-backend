0162fada1f8393542d09e4dd8b33dff260cc89de
Atish Shah Rouniyar <atishshahrouniyar@Atishs-MacBook-Air.local>
2024-10-12 22:11:29 -0500

    Add apps and models

---
+++ users/models.py
@@ -0,0 +1,44 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+import uuid
+
+
+class User(AbstractUser):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    name = models.CharField(max_length=255)
+    email = models.EmailField(unique=True)
+    password = models.CharField(max_length=255)
+    address = models.CharField(max_length=255)
+    phone = models.CharField(max_length=255)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    groups = models.ManyToManyField(
+        'auth.Group',
+        related_name='custom_user_groups',  # Changed related_name to avoid conflict
+        blank=True,
+    )
+    user_permissions = models.ManyToManyField(
+        'auth.Permission',
+        related_name='custom_user_permissions',  # Changed related_name to avoid conflict
+        blank=True,
+    )
+    
+
+
+class Patient(User):
+    blood_type = models.CharField(max_length=255)
+
+
+class Doctor(User):
+    specialization = models.CharField(max_length=255)
+
+    
+
+class OrganDonor(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    donor = models.ForeignKey(Patient, related_name='organs_donated', on_delete=models.CASCADE)
+    organ = models.CharField(max_length=255)
+    availability_status = models.CharField(max_length=255)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
