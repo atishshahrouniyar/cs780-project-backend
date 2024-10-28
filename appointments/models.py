97ad72a81607778453014ebca0da8df7c556b197
Atish Shah Rouniyar <atishshahrouniyar@Atishs-MacBook-Air.local>
2024-10-27 21:35:23 -0500

    Make patient id null

--- appointments/models.py
+++ appointments/models.py
@@ -1,11 +1,12 @@
 from django.db import models
 import uuid
 
+
 # Create your models here.
 class Appointment(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-    patient_id = models.ForeignKey('users.Patient', on_delete=models.CASCADE)
-    doctor_id = models.ForeignKey('users.Doctor', on_delete=models.CASCADE)
+    patient_id = models.ForeignKey("users.Patient", on_delete=models.CASCADE, null=True)
+    doctor_id = models.ForeignKey("users.Doctor", on_delete=models.CASCADE)
     appointment_date = models.DateTimeField()
     status = models.CharField(max_length=255)
     created_at = models.DateTimeField(auto_now_add=True)
