a81bd826cca94f0bb10e3056b00372703b9e97d4
Atish Shah Rouniyar <atishshahrouniyar@Atishs-MacBook-Air.local>
2024-11-10 19:24:05 -0600

    Add appointment information in medical record

--- medical_records/models.py
+++ medical_records/models.py
@@ -2,12 +2,17 @@ from django.db import models
 import uuid
 from users.models import Patient
 
+
 # Create your models here.
 class MedicalRecord(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-    appointment_id = models.ForeignKey('appointments.Appointment', on_delete=models.CASCADE)
+    appointment = models.ForeignKey(
+        "appointments.Appointment", on_delete=models.CASCADE
+    )
     diagnosis = models.TextField()
     prescription = models.TextField()
-    patient = models.ForeignKey(Patient, related_name='medical_records', on_delete=models.CASCADE)
+    patient = models.ForeignKey(
+        Patient, related_name="medical_records", on_delete=models.CASCADE
+    )
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
--- medical_records/serializers.py
+++ medical_records/serializers.py
@@ -1,8 +1,11 @@
 from rest_framework import serializers
 from .models import MedicalRecord
+from appointments.serializers import AppointmentSerializer
 
 
 class MedicalRecordSerializer(serializers.ModelSerializer):
+    appointment = AppointmentSerializer(read_only=True)
+
     class Meta:
         model = MedicalRecord
         fields = "__all__"
