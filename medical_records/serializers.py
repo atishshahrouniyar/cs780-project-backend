3c7d7ec103d2511247ac08ccfb6389d353b202df
Atish Shah Rouniyar <atishshahrouniyar@Atishs-MacBook-Air.local>
2024-11-11 09:57:07 -0600

    Update appointment id in medical records

--- medical_records/serializers.py
+++ medical_records/serializers.py
@@ -1,10 +1,14 @@
 from rest_framework import serializers
 from .models import MedicalRecord
 from appointments.serializers import AppointmentSerializer
+from appointments.models import Appointment
 
 
 class MedicalRecordSerializer(serializers.ModelSerializer):
     appointment = AppointmentSerializer(read_only=True)
+    appointment_id = serializers.PrimaryKeyRelatedField(
+        write_only=True, queryset=Appointment.objects.all(), source="appointment"
+    )
 
     class Meta:
         model = MedicalRecord
